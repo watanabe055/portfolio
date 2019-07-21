bab6eba69fae36dd07ef857794d7383b716e5c7c
watanabe055 <mailok1212@gmail.com>
2019-07-21 12:11:05 +0000

    new-css_my_room

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,7 @@
 class Room < ApplicationRecord
   has_many :messages
   enum category: { 喜び: 0, 悲しみ: 1, 不思議: 2, 憧れ: 3, 恐怖: 4, 後悔: 5, 期待: 6, 怒り: 7 }
+
+  validates :title, presence: true, length: { in: 1..100}
+  validates :detail, presence: true, length: { in: 1..200}
 end
