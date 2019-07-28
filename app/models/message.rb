ecf4835c3366a33e68cdfe968518eb73fc7421d6
watanabe055 <mailok1212@gmail.com>
2019-07-28 20:31:13 +0000

    create-chat_ng

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,4 +4,31 @@ class Message < ApplicationRecord
   belongs_to :room, optional: true
   # createの後にコミットする { MessageBroadcastJobのperformを遅延実行 引数はself }
   after_create_commit { MessageBroadcastJob.perform_later self }
+
+  #チャットの禁止ワード
+  INVALID_NAME_PARTS = %w(
+    ばか
+    バカ
+    馬鹿
+    間抜け
+    アホ
+    あほ
+    阿保
+    くたばれ
+    間抜け
+    マヌケ
+    まぬけ
+    ボケ
+    ぼけ
+    変態
+    死ね
+    くそ
+    クソ
+    デブ
+    ババア
+    キモい
+    しね
+    カス
+  )
+  validates :content, format: { without: ::Regexp.union(INVALID_NAME_PARTS) }
 end
